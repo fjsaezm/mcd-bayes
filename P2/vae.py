e05745e95e9841288466fe7abf7422e70ba58359
José Antonio Álvarez <joseantonioao32@gmail.com>
2022-05-05 17:13:28 +0200

    Update vae.py

--- P2/vae.py
+++ P2/vae.py
@@ -3,48 +3,63 @@
 import autograd.numpy as np
 import autograd.numpy.random as npr
 import autograd.scipy.stats.norm as norm
-    
+
 from autograd import grad
 from data import load_mnist
 from data import save_images as s_images
-from autograd.misc import flatten # This is used to flatten the params (transforms a list into a numpy array)
+from autograd.misc import (
+    flatten,
+)  # This is used to flatten the params (transforms a list into a numpy array)
 
 # images is an array with one row per image, file_name is the png file on which to save the images
 
-def save_images(images, file_name): return s_images(images, file_name, vmin = 0.0, vmax = 1.0)
+
+def save_images(images, file_name):
+    return s_images(images, file_name, vmin=0.0, vmax=1.0)
+
 
 # Sigmoid activiation function to estimate probabilities
+
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
+
 # Relu activation function for non-linearity
 
-def relu(x):    return np.maximum(0, x)
+
+def relu(x):
+    return np.maximum(0, x)
+
 
 # This function intializes the parameters of a deep neural network
 
-def init_net_params(layer_sizes, scale = 1e-2):
+
+def init_net_params(layer_sizes, scale=1e-2):
 
     """Build a (weights, biases) tuples for all layers."""
+    # layer_sizes is a list with the sizes of each layer
 
-    return [(scale * npr.randn(m, n),   # weight matrix
-             scale * npr.randn(n))      # bias vector
-            for m, n in zip(layer_sizes[:-1], layer_sizes[1:])]
+    return [
+        (scale * npr.randn(m, n), scale * npr.randn(n))  # weight matrix  # bias vector
+        for m, n in zip(layer_sizes[:-1], layer_sizes[1:])
+    ]
+
 
 # This will be used to normalize the activations of the NN
 
 # This computes the output of a deep neuralnetwork with params a list with pairs of weights and biases
 
+
 def neural_net_predict(params, inputs):
 
     """Params is a list of (weights, bias) tuples.
-       inputs is an (N x D) matrix.
-       Applies batch normalization to every layer but the last."""
+    inputs is an (N x D) matrix.
+    Applies batch normalization to every layer but the last."""
 
     for W, b in params[:-1]:
         outputs = np.dot(inputs, W) + b  # linear transformation
-        inputs = relu(outputs)         # nonlinear transformation
+        inputs = relu(outputs)  # nonlinear transformation
 
     # Last layer is linear
 
@@ -53,7 +68,9 @@ def neural_net_predict(params, inputs):
 
     return outputs
 
+
 # This implements the reparametrization trick
+
 
 def sample_latent_variables_from_posterior(encoder_output):
 
@@ -68,7 +85,9 @@ def sample_latent_variables_from_posterior(encoder_output):
 
     return 0.0
 
+
 # This evlauates the log of the term that depends on the data
+
 
 def bernoulli_log_prob(targets, logits):
 
@@ -76,15 +95,19 @@ def bernoulli_log_prob(targets, logits):
     # Targets must be between 0 and 1
 
     # TODO compute the log probability of the targets given the generator output specified in logits
-    # sum the probabilities across the dimensions of each image in the batch. The output of this function 
+    # sum the probabilities across the dimensions of each image in the batch. The output of this function
     # should be a vector of size the batch size
+
+    # THIS IS EQUATION 15 FROM THE PDF
 
     return 0.0
 
+
 # This evaluates the KL between q and the prior
 
+
 def compute_KL(q_means_and_log_stds):
-    
+
     D = np.shape(q_means_and_log_stds)[-1] // 2
     mean, log_std = q_means_and_log_stds[:, :D], q_means_and_log_stds[:, D:]
 
@@ -94,14 +117,16 @@ def compute_KL(q_means_and_log_stds):
 
     return 0.0
 
+
 # This evaluates the lower bound
+
 
 def vae_lower_bound(gen_params, rec_params, data):
 
     # TODO compute a noisy estiamte of the lower bound by using a single Monte Carlo sample:
 
     # 1 - compute the encoder output using neural_net_predict given the data and rec_params
-    # 2 - sample the latent variables associated to the batch in data 
+    # 2 - sample the latent variables associated to the batch in data
     #     (use sample_latent_variables_from_posterior and the encoder output)
     # 3 - use the sampled latent variables to reconstruct the image and to compute the log_prob of the actual data
     #     (use neural_net_predict for that)
@@ -111,19 +136,19 @@ def vae_lower_bound(gen_params, rec_params, data):
     return 0.0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # Model hyper-parameters
 
-    npr.seed(0) # We fix the random seed for reproducibility
+    npr.seed(0)  # We fix the random seed for reproducibility
 
     latent_dim = 50
     data_dim = 784  # How many pixels in each image (28x28).
     n_units = 200
     n_layers = 2
 
-    gen_layer_sizes = [ latent_dim ] + [ n_units for i in range(n_layers) ] + [ data_dim ]
-    rec_layer_sizes = [ data_dim ]  + [ n_units for i in range(n_layers) ] + [ latent_dim * 2 ]
+    gen_layer_sizes = [latent_dim] + [n_units for i in range(n_layers)] + [data_dim]
+    rec_layer_sizes = [data_dim] + [n_units for i in range(n_layers)] + [latent_dim * 2]
 
     # Training parameters
 
@@ -143,7 +168,7 @@ if __name__ == '__main__':
 
     init_rec_params = init_net_params(rec_layer_sizes)
 
-    combined_params_init = (init_gen_params, init_rec_params) 
+    combined_params_init = (init_gen_params, init_rec_params)
 
     num_batches = int(np.ceil(len(train_images) / batch_size))
 
@@ -160,12 +185,14 @@ if __name__ == '__main__':
         gen_params, rec_params = combined_params
 
         # We binarize the data
-    
-        on = train_images[ data_idx ,: ] > npr.uniform(size = train_images[ data_idx ,: ].shape)
-        images = train_images[ data_idx, : ] * 0.0
-        images[ on ] = 1.0
 
-        return vae_lower_bound(gen_params, rec_params, images) 
+        on = train_images[data_idx, :] > npr.uniform(
+            size=train_images[data_idx, :].shape
+        )
+        images = train_images[data_idx, :] * 0.0
+        images[on] = 1.0
+
+        return vae_lower_bound(gen_params, rec_params, images)
 
     # Get gradients of objective using autograd.
 
@@ -173,7 +200,7 @@ if __name__ == '__main__':
     flattened_current_params = flattened_combined_params_init
 
     # ADAM parameters
-    
+
     t = 1
 
     # TODO write here the initial values for the ADAM parameters (including the m and v vectors)
@@ -187,11 +214,14 @@ if __name__ == '__main__':
 
         for n_batch in range(int(np.ceil(N / batch_size))):
 
-            batch = np.arange(batch_size * n_batch, np.minimum(N, batch_size * (n_batch + 1)))
+            batch = np.arange(
+                batch_size * n_batch, np.minimum(N, batch_size * (n_batch + 1))
+            )
+            # TODO: nota de drejo, esto es el gradiente ruidosos
             grad = objective_grad(flattened_current_params)
 
             # TODO Use the estimated noisy gradient in grad to update the paramters using the ADAM updates
-    
+
             elbo_est += objective(flattened_current_params)
 
             t += 1
@@ -204,20 +234,18 @@ if __name__ == '__main__':
 
     # TODO Generate 25 images from prior (use neural_net_predict) and save them using save_images
 
-    # TODO Generate image reconstructions for the first 10 test images (use neural_net_predict for each model) 
+    # TODO Generate image reconstructions for the first 10 test images (use neural_net_predict for each model)
     # and save them alongside with the original image using save_images
-        
+
     num_interpolations = 5
     for i in range(5):
 
-        # TODO Generate 5 interpolations from the first test image to the second test image, 
+        # TODO Generate 5 interpolations from the first test image to the second test image,
         # for the third to the fourth and so on until 5 interpolations
-        # are computed in latent space and save them using save images. 
+        # are computed in latent space and save them using save images.
         # Use a different file name to store the images of each iterpolation.
         # To interpolate from  image I to image G use a convex conbination. Namely,
         # I * s + (1-s) * G where s is a sequence of numbers from 0 to 1 obtained by numpy.linspace
         # Use mean of the recognition model as the latent representation.
 
         pass
-
-
